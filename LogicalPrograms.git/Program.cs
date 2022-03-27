a6e13bc30b6f5f2a3b63f63d53b9b6fec962a46d
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-27 10:47:14 +0530

    [Wasim] Add: Find Perfect number

--- LogicalPrograms.git/Program.cs
+++ LogicalPrograms.git/Program.cs
@@ -12,18 +12,18 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Enter the value of Number");
             int n = Convert.ToInt32(Console.ReadLine());
-            int countFactors = 0;
-            for (int i = 1; i <= n; i++)
+            int sum = 0;
+            for (int i = 1; i < n; i++)
             {
                 if (n % i == 0)
                 {
-                    countFactors++;
+                    sum = sum + i;
                 }
             }
-            if (countFactors == 2)
-                Console.WriteLine("Prime No");
+            if (sum == n)
+                Console.WriteLine("Perfect No");
             else
-                Console.WriteLine("Not Prime No");
+                Console.WriteLine("Not Perfect No");
         }
     }
 }
