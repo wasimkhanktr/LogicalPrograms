f66736dc47b53cecba238a83b8dd31d73c0e01cb
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-27 10:36:29 +0530

    [Wasim] Add: Reverse no

---
+++ LogicalPrograms.git/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of Number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int reverseNo = 0;
+            while (n > 0)
+            {
+                int d = n % 10;
+                reverseNo = reverseNo * 10 + d;
+                n = n / 10;
+            }
+            Console.WriteLine("Reverse Number : "+ reverseNo);
+        }
+    }
+}
